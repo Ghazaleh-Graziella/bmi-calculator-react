9997ed97a04c793929dddc12e224a10a960396c1
Ghazaleh-Graziella <ghazale.f@yahoo.com>
2022-12-01 13:38:31 +0330

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/App/App.jsx';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+ReactDOM.render(<App />, document.getElementById('root'));
+
+serviceWorkerRegistration.register();
