7363c79af32b62817d03c80b17bafe40949352d0
Ghazaleh-Graziella <ghazale.f@yahoo.com>
2022-12-01 13:39:37 +0330

    Updates

---
+++ precache-manifest.353c984f63bbb349aaba371ed5c5eb82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b21b90631152552f12831014769aa0a8",
+    "url": "/bmi-calculator/index.html"
+  },
+  {
+    "revision": "b4aa88851593ac880bfa",
+    "url": "/bmi-calculator/static/css/2.ae9e8b99.chunk.css"
+  },
+  {
+    "revision": "98eef29a8875fb3a4db2",
+    "url": "/bmi-calculator/static/css/main.fce36487.chunk.css"
+  },
+  {
+    "revision": "b4aa88851593ac880bfa",
+    "url": "/bmi-calculator/static/js/2.0e5f39ad.chunk.js"
+  },
+  {
+    "revision": "98eef29a8875fb3a4db2",
+    "url": "/bmi-calculator/static/js/main.9d6e08e8.chunk.js"
+  },
+  {
+    "revision": "9b25b4e690523b173542",
+    "url": "/bmi-calculator/static/js/runtime~main.425d477f.js"
+  }
+]);
